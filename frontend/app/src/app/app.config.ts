7112ac9fc69a033c431c1ccfcaeb68a8c7f434d6
Ahad <ahadtursyn07@gmail.com>
2026-04-17 23:51:52 +0500

    UI update

--- frontend/app/src/app/app.config.ts
+++ frontend/app/src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
